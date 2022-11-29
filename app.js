282a949da2208b491bb3d90bae97bfb57eedffa3
Donovan <donovangriffin04@gmail.com>
2022-11-29 12:54:51 -0800

    refactored mouseover effect

--- app.js
+++ app.js
@@ -14,33 +14,15 @@ burger.addEventListener('click', function(){
 
 
 const images = document.querySelector('.gallery-img')
-
+const imgIdentity = document.querySelector('.img-identifier')
 
 
 const imgHover = (e) => {
   let name = e.target.id
-  if (name === 'one'){
-    document.querySelector('#one').style.width = '110%'
-  }else if (name === 'two'){
-    document.querySelector('#two').style.width = '110%'
-  }else if (name === 'three'){
-    document.querySelector('#three').style.width = '110%'
-  }else if (name === 'four'){
-    document.querySelector('#four').style.width = '110%'
-  }else if (name === 'five'){
-    document.querySelector('#five').style.width = '110%'
-  }else if (name === 'six'){
-    document.querySelector('#six').style.width = '110%'
-  }else if (name === 'seven'){
-    document.querySelector('#seven').style.width = '110%'
-  }else if (name === 'eight'){
-    document.querySelector('#eight').style.width = '110%'
-  }else if (name === 'nine'){
-    document.querySelector('#nine').style.width = '110%'
-  }else if (name === 'ten'){
-    document.querySelector('#ten').style.width = '110%'
+    document.querySelector(`#${name}`).style.width = '110%'
+ 
   }
-}
+
   const imgDown = (e) => {
     let name = e.target.id
     if (name === 'one'){
