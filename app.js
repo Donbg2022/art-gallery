1ce22b209508ead36a4c126711b00e2f6ccacfd2
Donovan <donovangriffin04@gmail.com>
2022-11-28 19:06:18 -0800

    started site

---
+++ app.js
@@ -0,0 +1,12 @@
+const burger = document.querySelector('#burger')
+let click = 1
+//add event listener to make a drop down menu of options
+burger.addEventListener('click', function(){
+  //adds 1 to click variable and uses if statement to open and close the links 
+  click++
+  if (click % 2 !== 0){
+    return document.querySelector('#reactivelinks').style.display = 'none'
+  }else{
+    return document.querySelector('#reactivelinks').style.display = 'block'
+  }
+})
