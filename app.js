4085f5ccdc9715818101beb0a7772dae3fea9c4b
Donovan <donovangriffin04@gmail.com>
2022-11-29 13:20:49 -0800

    refactored mouseover and mousedown to use significantly less code

--- app.js
+++ app.js
@@ -19,34 +19,16 @@ const imgIdentity = document.querySelector('.img-identifier')
 
 const imgHover = (e) => {
   let name = e.target.id
+  if (name.length !== 0){ 
     document.querySelector(`#${name}`).style.width = '110%'
- 
-  }
 
+  }
+}
   const imgDown = (e) => {
     let name = e.target.id
-    if (name === 'one'){
-      document.querySelector('#one').style.width = '100%'
-    }else if (name === 'two'){
-      document.querySelector('#two').style.width = '100%'
-    }else if (name === 'three'){
-      document.querySelector('#three').style.width = '100%'
-    }else if (name === 'four'){
-      document.querySelector('#four').style.width = '100%'
-    }else if (name === 'five'){
-      document.querySelector('#five').style.width = '100%'
-    }else if (name === 'six'){
-      document.querySelector('#six').style.width = '100%'
-    }else if (name === 'seven'){
-      document.querySelector('#seven').style.width = '100%'
-    }else if (name === 'eight'){
-      document.querySelector('#eight').style.width = '100%'
-    }else if (name === 'nine'){
-      document.querySelector('#nine').style.width = '100%'
-    }else if (name === 'ten'){
-      document.querySelector('#ten').style.width = '100%'
-    }
-
+    if (name.length !== 0){ 
+      document.querySelector(`#${name}`).style.width = '100%'
+  }
 }
 images.addEventListener('mouseover', imgHover)
 images.addEventListener('mouseout', imgDown)
