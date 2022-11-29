5083ae6797f8492169e959f37a07c564f7727212
Donovan <donovangriffin04@gmail.com>
2022-11-29 08:52:56 -0800

    added mouseover and out effect

--- app.js
+++ app.js
@@ -18,18 +18,53 @@ const images = document.querySelector('.gallery-img')
 
 
 const imgHover = (e) => {
-  console.log(e.toElement.name)
-  if (e.toElement.name === '1'){
-    console.log('yes')
-  }else{
-    console.log('no')
+  let name = e.target.id
+  if (name === 'one'){
+    document.querySelector('#one').style.width = '110%'
+  }else if (name === 'two'){
+    document.querySelector('#two').style.width = '110%'
+  }else if (name === 'three'){
+    document.querySelector('#three').style.width = '110%'
+  }else if (name === 'four'){
+    document.querySelector('#four').style.width = '110%'
+  }else if (name === 'five'){
+    document.querySelector('#five').style.width = '110%'
+  }else if (name === 'six'){
+    document.querySelector('#six').style.width = '110%'
+  }else if (name === 'seven'){
+    document.querySelector('#seven').style.width = '110%'
+  }else if (name === 'eight'){
+    document.querySelector('#eight').style.width = '110%'
+  }else if (name === 'nine'){
+    document.querySelector('#nine').style.width = '110%'
+  }else if (name === 'ten'){
+    document.querySelector('#ten').style.width = '110%'
   }
-
-  //possibly use case/switch instead of if
-
-
-  img.style.width === 200px
-  
+}
+  const imgDown = (e) => {
+    let name = e.target.id
+    if (name === 'one'){
+      document.querySelector('#one').style.width = '100%'
+    }else if (name === 'two'){
+      document.querySelector('#two').style.width = '100%'
+    }else if (name === 'three'){
+      document.querySelector('#three').style.width = '100%'
+    }else if (name === 'four'){
+      document.querySelector('#four').style.width = '100%'
+    }else if (name === 'five'){
+      document.querySelector('#five').style.width = '100%'
+    }else if (name === 'six'){
+      document.querySelector('#six').style.width = '100%'
+    }else if (name === 'seven'){
+      document.querySelector('#seven').style.width = '100%'
+    }else if (name === 'eight'){
+      document.querySelector('#eight').style.width = '100%'
+    }else if (name === 'nine'){
+      document.querySelector('#nine').style.width = '100%'
+    }else if (name === 'ten'){
+      document.querySelector('#ten').style.width = '100%'
+    }
 
 }
 images.addEventListener('mouseover', imgHover)
+images.addEventListener('mouseout', imgDown)
