7958909ccfe2dceb16a06132b690c0796ced522c
Donovan <donovangriffin04@gmail.com>
2022-12-07 18:34:39 -0800

    fixed glitch where text appears and goes away rapidly like flashing

--- src/order.js
+++ src/order.js
@@ -8,18 +8,10 @@ const imgHover = (e) => {
 
   }
 }
-  const imgDown = (e) => {
-    let name = e.target.id
-    console.log(e)
-    if (name.length !== 0){ 
-      document.querySelector(`.${name}`).style.display = 'none'
+//mouseover affect to make text disapear when cursor moves from img
+window.addEventListener('mouseover', (e) => {
+  console.log(e)
+  if(e.target.nodeName === 'BODY'){
+    document.querySelector('.buying-info').style.display = 'none'
   }
-}
-
-//initializing eventlisteners
-imgList.addEventListener('mouseover', imgHover)
-imgList.addEventListener('mouseout', imgDown)
-
-
-
-// the hover effect is detecting the div which is why it is glitching out
+})
