7f2d65eea7a926fae83970784ff59f90cdcbe7f6
Donovan <donovangriffin04@gmail.com>
2022-12-09 17:15:40 -0800

    fixed small navbar useability

--- src/order.js
+++ src/order.js
@@ -1,3 +1,16 @@
+const burger = document.querySelector('#burger')
+let click = 1
+//add event listener to make a drop down menu of options
+burger.addEventListener('click', function(){
+  //adds 1 to click variable and uses if statement to open and close the links 
+  click++
+  if (click % 2 !== 0){
+    return document.querySelector('#reactivelinks').style.display = 'none'
+  }else{
+    return document.querySelector('#reactivelinks').style.display = 'block'
+  }
+})
+
 const imgList = document.querySelector('.img-list')
 
 //event listener functions to make hover over images enlarge image and display id
